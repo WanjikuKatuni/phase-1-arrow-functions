e5341d9cda39eb4091c2625ca759f69cdfa7340f
wanjiku <annritawanjiku77@gmail.com>
2022-08-19 22:41:45 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (num1,num2) => {
+    
+
+    return parseInt(num1)/parseInt(num2);
+}
+console.log(divide(2000,100));
+
+const square = num3 => {
+    return num3*num3
+}
+square(2);
+
+const add = (num4, num5) =>{
+    return num4+num5;
+}
+add(6,7);
